4027427c574f34b5da3af0bfbd6aaf7e69ac3198
Franciszek Andruszkiewicz <fandruszkiewicz@gmail.com>
2018-02-05 00:29:02 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="dist/main.css">
+  <title>BEEF cms - login</title>
+</head>
+<body>
+  <div class="loginScreen">
+  <div class="loginScreen__wrapper">
+  <img src="img/svg/logo.svg" alt="beef.cms" class="svg svg--logo">
+  <label for="login">Login</label>
+  <input type="text" id="login">
+  <label for="password">Password</label>
+  <input type="password" id="password">
+  <input type="submit" value="Login" class="login__button">
+  </div>
+  </div>
+</body>
+</html>
